65d311e10ead91690fd3c9dd549e420f224340cc
alcm1870 <131430000+alcm1870@users.noreply.github.com>
2023-04-24 22:51:54 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -126,8 +126,6 @@ def test(checkpoint_path,img, model,img_hw):
 def checkpoint_down():
     import oss2
     import os
-    # 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-    auth = oss2.Auth('LTAI5tDfyPSxSpJNCUXbg2RS', 'xmgpMijSsP11xL2R25ArX64RTfUVUT')
     # Endpoint以杭州为例，其它Region请按实际情况填写。
     bucket = oss2.Bucket(auth, 'oss-cn-chengdu.aliyuncs.com', 'model-pth')
 
